5574297f60169802843882a99e6b86476ce13ee8
jefedeltejas <jeff.jackson@dashiell.com>
2017-03-22 15:50:38 -0500

    outputting address and grid coords

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=12301%20kurland%20drive%20houston',
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(response, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
