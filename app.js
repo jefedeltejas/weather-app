255e8f2ff4216c029757481c0435af4964d4917e
jefedeltejas <jeff.jackson@dashiell.com>
2017-03-22 15:28:34 -0500

    changed output to http request data + the body

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=12301%20kurland%20drive%20houston',
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
+  console.log(JSON.stringify(response, undefined, 2));
 });
