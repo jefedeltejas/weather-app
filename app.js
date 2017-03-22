8d8029dd093f48259b05d6c23ebb82045521156a
jefedeltejas <jeff.jackson@dashiell.com>
2017-03-22 11:55:11 -0500

    stringify'd json output

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=12301%20kurland%20drive%20houston',
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  console.log(JSON.stringify(body, undefined, 2));
 });
