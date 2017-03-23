8a434a72766fb5d0f0dab2fdeb7a573cf3ee73fd
jefedeltejas <jeff.jackson@dashiell.com>
2017-03-23 14:31:48 -0500

    added error handling for address search

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const request = require('request'),
+      utils = require('utils'),
       yargs = require('yargs'),
       argv = yargs.options({
         a: {
@@ -12,11 +13,21 @@ const request = require('request'),
       .alias('help', 'h')
       .argv;
 
+console.log(argv);
+
+var encodedAddr = encodeURIComponent(argv.address);
+
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=12301%20kurland%20drive%20houston',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddr}`,
   json: true
 }, (error, response, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  if (error) {
+    console.log('Unable to connect to the great Google.');
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('Address not found ?!?');
+  } else if (body.status === 'OK'){
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  }
 });
