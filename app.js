25783e0a8528752ccaaff97b81dfdd3b19aed8ff
jefedeltejas <jeff.jackson@dashiell.com>
2017-03-24 13:13:02 -0500

    calling darksky with error handling

--- app.js
+++ app.js
@@ -26,11 +26,19 @@
 
 const request = require('request');
 
-var key = e9164075d1f40ad93b12e564d7f960d7,  //dark sky api key
+var key = 'e9164075d1f40ad93b12e564d7f960d7',  //dark sky api key
     lat = 29.6149269,
     lng = -95.2060879;
 
 request({
   url: `https://api.darksky.net/forecast/${key}/${lat},${lng}`,
   json: true
+}, (error, response, body) => {
+  if (error) {
+    console.log('Unable to connect to weather data source.');
+  } else if (response.statusCode === 400) {
+    console.log('Unable to get your forecast. Try again later.');
+  } else if (response.statusCode === 200) {
+    console.log(body.currently.temperature);
+  }
 });
