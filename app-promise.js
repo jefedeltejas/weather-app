84cf6e81ba9466527ff3da961bfc4283c0b2a89f
jefedeltejas <jeff.jackson@dashiell.com>
2017-03-27 14:32:11 -0500

    getting the promise structure to function correctly on app-promise.js

--- app-promise.js
+++ app-promise.js
@@ -12,5 +12,19 @@ const yargs = require('yargs'),
       .alias('help', 'h')
       .argv;
 
+var encodedAddr = encodeURIComponent(argv.address);
+var geocodUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddr}`;
+
+axios.get(geocodUrl).then((response) => {
+  console.log(response.data);
+}).catch((e) => {
+  if (e.code === 'ENOTFOUND') {
+    console.log('Unable to connect to that google API, G. Gonna halfta run a check on your request.');
+  }
+  console.log(e);
+});
+
+
+
 // var lat = geocode.results.latitude,
 //     lng = geocode.results.longitude;
