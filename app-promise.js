9c58bf86162224bcd78c46ab4670f6cb26a6fb62
jefedeltejas <jeff.jackson@dashiell.com>
2017-03-27 15:07:09 -0500

    This the end of content from the online course, but I can expand the functionality later

--- app-promise.js
+++ app-promise.js
@@ -16,12 +16,26 @@ var encodedAddr = encodeURIComponent(argv.address);
 var geocodUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddr}`;
 
 axios.get(geocodUrl).then((response) => {
-  console.log(response.data);
+  if (response.data.status === 'ZERO_RESULTS') {
+    throw new Error('Sorry player, can\'t find that address fo ya.');
+  }
+
+  var key = 'e9164075d1f40ad93b12e564d7f960d7',  //dark sky api key
+      lat = response.data.results[0].geometry.location.lat,
+      lng = response.data.results[0].geometry.location.lng,
+      weatherUrl = `https://api.darksky.net/forecast/${key}/${lat},${lng}`;
+  console.log(response.data.results[0].formatted_address);
+  return axios.get(weatherUrl);
+}).then((response) => {
+  var temp = response.data.currently.temperature,
+      feelsLike = response.data.currently.apparentTemperature;
+      console.log(`It's currently ${temp}  It feels like ${feelsLike}`);
 }).catch((e) => {
   if (e.code === 'ENOTFOUND') {
     console.log('Unable to connect to that google API, G. Gonna halfta run a check on your request.');
+  } else {
+      console.log(e.message);
   }
-  console.log(e);
 });
 
 
